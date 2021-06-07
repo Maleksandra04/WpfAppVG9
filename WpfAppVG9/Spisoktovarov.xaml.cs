72388d6019de79dd24397ae9aa43273a912f1595
Cab39-5 <Cab39-5@DESKTOP-UI8LRI2>
2021-06-07 11:37:50 +0400

    Добавьте файлы проекта.

---
+++ WpfAppVG9/Spisoktovarov.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppVG9
+{
+    /// <summary>
+    /// Логика взаимодействия для Spisoktovarov.xaml
+    /// </summary>
+    public partial class Spisoktovarov : Window
+    {
+        public Spisoktovarov()
+        {
+            InitializeComponent();
+        }
+
+        private void Tovar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dobavlenie add = new Dobavlenie();
+            add.Show();
+        }
+
+        private void delButton_Click(object sender, RoutedEventArgs e)
+        {
+            Ydalit del = new Ydalit();
+            del.Show();
+            this.Close();
+        }
+
+        private void findButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void printButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
